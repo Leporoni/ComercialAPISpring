5dba483abe563226a4534726757a6e4e3f2f0d39
Leporoni <leporoni@hotmail.com>
2019-01-31 09:33:01 -0200

    first commit

---
+++ src/main/java/com/leporonitech/comercial/repository/OportunidadeRepository.java
@@ -0,0 +1,9 @@
+package com.leporonitech.comercial.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.leporonitech.comercial.model.Oportunidade;
+
+public interface OportunidadeRepository extends JpaRepository<Oportunidade, Long> {
+
+}
