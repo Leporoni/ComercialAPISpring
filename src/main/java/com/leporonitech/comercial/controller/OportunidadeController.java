b49df071e3a002eea1dae89dc61bae96ef38079d
Leporoni <leporoni@hotmail.com>
2019-01-31 10:08:47 -0200

    first commit

--- src/main/java/com/leporonitech/comercial/controller/OportunidadeController.java
+++ src/main/java/com/leporonitech/comercial/controller/OportunidadeController.java
@@ -3,6 +3,8 @@ package com.leporonitech.comercial.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.leporonitech.comercial.model.Oportunidade;
 import com.leporonitech.comercial.repository.OportunidadeRepository;
@@ -42,7 +45,14 @@ public class OportunidadeController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Oportunidade adicionar(@RequestBody Oportunidade oportunidade) {
+	public Oportunidade adicionar(@Valid @RequestBody Oportunidade oportunidade) {
+		Optional<Oportunidade> oportunidadeExistente = oportunidades
+				.findByDescricaoAndNomeProspecto(oportunidade.getDescricao(), oportunidade.getNomeProspecto());
+		
+		if(oportunidadeExistente.isPresent()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+					"Já existe uma oportunidade para este prospecto com a mesma descrição.");
+		}
 		return oportunidades.save(oportunidade);
 	}
 
